44a85dc05366e8e212da79be15267dd7a954eabd
Ariel Fischer <ariel.fischer@qapio.com>
2024-05-10 20:16:43 +0200

    * Add Scheduler.py

--- src/qapi_python/actors/Source.py
+++ src/qapi_python/actors/Source.py
@@ -5,11 +5,23 @@ import pykka
 from qapi_python.client.Qapi import QapioGrpcInstance
 
 
+class Event:
+    def __init__(self, inlet: str, value: Any):
+        self.__inlet = inlet
+        self.__value = value
+
+    @property
+    def inlet(self):
+        return self.__inlet
+
+    @property
+    def value(self):
+        return self.__value
+
+
 class Source(pykka.ThreadingActor):
     def __init__(self, expression: str, client: QapioGrpcInstance, actor_ref: pykka.ActorRef, *_args: Any, **_kwargs: Any):
         super().__init__(*_args, **_kwargs)
         self.__client = client
-        print("JEE!")
-        client.source(expression).subscribe(lambda x: actor_ref.tell(x))
-
+        client.source(expression).subscribe(lambda x: actor_ref.tell(Event(expression, x)))
 
---
+++ src/qapi_python/stages/Function.py
@@ -0,0 +1,46 @@
+from typing import Any
+from pykka import ThreadingActor
+import os
+import json
+from qapi_python.actors import Qapi as QapiActor
+
+from src.qapi_python.actors.Source import Event
+
+
+class FlowActor(ThreadingActor):
+    def __init__(self, qapi, manifest, func, *_args: Any, **_kwargs: Any):
+        super().__init__(*_args, **_kwargs)
+        self.__manifest = manifest
+        self.__function = func
+
+        qapi.proxy().source(manifest["inlets"]["Request"], self.actor_ref)
+        qapi.proxy().source(manifest["inlets"]["Code"], self.actor_ref)
+
+        self.__sink = qapi.proxy().sink(manifest["outlets"]["Response"]).get().proxy()
+
+    def transmit(self, value):
+        self.__sink.on_next(self.__function(value))
+
+    def on_receive(self, message: Event) -> Any:
+
+        if message.inlet is self.__manifest["inlets"]["Request"]:
+            self.transmit(message.value)
+
+        if message.inlet is self.__manifest["inlets"]["Code"]:
+            print(message.value)
+
+
+def function(fn):
+
+    endpoint = "127.0.0.1:5021"
+
+    qapi = QapiActor.Qapi.start(endpoint)
+
+    manifest_length = int.from_bytes(
+        os.read(0, 4),
+        "little"
+    )
+
+    manifest = json.loads(os.read(0, manifest_length).decode('utf8'))
+
+    FlowActor.start(qapi, manifest, fn)
