44de56f95856eabb2fb755015955c4a59db98134
Ariel Fischer <ariel.fischer@qapio.com>
2024-05-18 17:20:00 +0200

    * Add universe

--- src/qapi_python/client/Qapi.py
+++ src/qapi_python/client/Qapi.py
@@ -110,7 +110,11 @@ class Transmitter:
         chunks = create_chunks(value, 64000)
         for chunk in chunks:
             v = json.dumps(chunk, default=custom_encoder)
-            c = qapi_pb2.Chunk(expression=self.__expression, bytes=[Any(value=bytes(v, 'utf-8'))])
+            payload_bytes = bytes(v, 'utf-8')
+            payload_length = len(payload_bytes).to_bytes(4, byteorder='big')
+        #return [payload_length + payload_bytes]
+            data = payload_length + payload_bytes
+            c = qapi_pb2.Chunk(expression=self.__expression, bytes=[Any(value=data)])
             yield c
 
     def on_next(self, value):
